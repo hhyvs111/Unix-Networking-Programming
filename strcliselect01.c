b5129093bdd487cc17e1e682d93c6fedf3bc39a5
tanwenbo <445622919@qq.com>
2018-05-25 19:03:53 +0800

    test

--- strcliselect01.c
+++ strcliselect01.c
@@ -10,7 +10,7 @@ str_cli(FILE *fp, int sockfd)		//这里是fp，应该是从文件读内容到soc
 	FD_ZERO(&rset);		//初始化该集合
 	for ( ; ; ) {
 		//The function fileno() examines the argument stream and returns its integer descriptor.
-		
+
 		FD_SET(fileno(fp), &rset);	//fileno函数把标准I/O文件指针转换为对应的描述符
 		FD_SET(sockfd, &rset);		//FD_SET打开两位
 		/*
@@ -19,7 +19,7 @@ str_cli(FILE *fp, int sockfd)		//这里是fp，应该是从文件读内容到soc
 	*/
 		maxfdp1 = max(fileno(fp), sockfd) + 1;
 		Select(maxfdp1, &rset, NULL, NULL, NULL);
-		
+
 		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
 			if (Readline(sockfd, recvline, MAXLINE) == 0)
 				err_quit("str_cli: server terminated prematurely");
